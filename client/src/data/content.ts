39c4a3246497e501dc9f0713959d765681291416
Sakura <sakuragrapefruit@gmail.com>
2026-05-14 13:53:56 -0700

    fixed some styling

--- client/src/data/content.ts
+++ client/src/data/content.ts
@@ -3,12 +3,15 @@
 
 import type { Interest, Project } from '../types';
 import Sylveon from '../assets/sylveon.jpg'
+import Gwen from '../assets/gwen.jpg'
+import Oppossum from '../assets/opossum.gif'
+import Benny from '../assets/benny.jpg'
 
 export const interests: Interest[] = [
-  { title: 'Pokémon', images: Sylveon, description: 'Been a fan since I watched Indigo League for the first time', favorites: ['Sylveon', 'Mareep/Mega Evolution Ampharos', 'Abra'] },
-  { title: 'Art', images: '', description: 'I enjoy both digital and traditional art, but I am too insecure about it to post really.', favorites: ['Watercolor', 'Pixel Art', 'Illustration'] },
-  { title: 'Animals', images: '', description: 'A huge animal lover overall.', favorites: ['Red Panda', 'Axolotl', 'Snow Leopard'] },
-  { title: 'Cats', images: '', description: 'Cats are my favorite animal.', favorites: ['Maine Coon', 'Scottish Fold', 'Ragdoll'] },
+  { title: 'Pokémon', images: Sylveon, description: 'Been a fan since I watched Indigo League for the first time', favorites: ['Sylveon', 'Mareep', 'Abra', 'Vaporeon', 'Litten', 'Mega Evolution Ampharos'] },
+  { title: 'Art', images: Gwen, description: 'I enjoy both digital and traditional art, but I am too insecure about it to post really.', favorites: ['Graphite Pencil', 'Oil Paint', 'Acrylic Marker'] },
+  { title: 'Animals', images: Oppossum, description: 'A huge animal lover overall.', favorites: ['Cheetah', 'Sand Cat', 'Burrowing Owl', 'Barn Owl', 'Oppossums', 'Pangolin'] },
+  { title: 'Cats', images: Benny, description: 'Cats are my favorite animal.', favorites: ['Orange Cat', 'Tuxedo Cat', 'Bombay Cat'] },
 ];
 
 export const projects: Project[] = [
