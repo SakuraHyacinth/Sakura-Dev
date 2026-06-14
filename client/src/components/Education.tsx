5efbe66fcb6b619a64ef92922f2528fc4b1c533a
Sakura <sakuragrapefruit@gmail.com>
2026-06-14 13:49:00 -0700

    everything looks terrible

--- client/src/components/Education.tsx
+++ client/src/components/Education.tsx
@@ -1,7 +1,8 @@
 import GitHub from "../assets/github.png"
+import LinkedIn from "../assets/linkedin.png"
 
 const Education = () => (
-  <section id="about" className="about">
+  <section id="education" className="education">
     <h2>Education</h2>
     <h3>California State University, San Marcos (CSUSM)</h3>
     <p>Bachelors of Science (BS) - Computer Science</p>
@@ -9,7 +10,10 @@ const Education = () => (
     <p>GPA: 3.8</p>
     <p>Deans list: Fall 2022 - Spring 2026</p>
     <h2>Skills</h2>
-    <img src={GitHub} className="icon-img" />
+    <div id="icon-div">
+      <img src={GitHub} className="icon-img" />
+      <img src={LinkedIn} className="icon-img" />
+    </div>
   </section>
 );
 
